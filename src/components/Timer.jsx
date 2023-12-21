19ab921b6101fe9d935b836cc81112a282db98bc
harryudi <udiharrison23@gmail.com>
2023-12-21 12:11:43 +0100

    ok

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Timer({setStopGame, questionNumber}) {
     const [timer, setTimer]=useState(30)
